6bb46d4a864b6e93fffd5c09bc3e3ec9288a7f02
rickzx98 <rickzx98@gmail.com>
2017-05-31 11:22:07 +0800

    adding cache feature

--- src/chain/ChainStorage.js
+++ src/chain/ChainStorage.js
@@ -1,3 +1,4 @@
+import { generateUUID } from './Util';
 import lodash from 'lodash';
 
 export const ChainStorage = {};
@@ -6,9 +7,22 @@ export const putChain = (name, chain) => {
     lodash.set(ChainStorage, name, () => chain);
 };
 
+export const createChainState = (chain) => {
+    const key = generateUUID();
+    lodash.set(ChainStorage, key, {
+        root: Chain
+    });
+}
+
 export const getMiddlewares = () => {
     return lodash.filter(ChainStorage, (storage) => {
         const chain = storage();
         return chain.type && chain.type === 'MIDDLEWARE';
     });
+}
+
+class ChainState  {
+    constructor(chain, name, param){
+        
+    }
 }
